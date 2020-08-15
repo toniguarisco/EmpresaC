d19d9f9f3387294dd6f6aa25aad8012e6cbb4104
Marco Licini <Marcolicini45@gmail.com>
2020-08-15 17:42:40 -0400

    mejora metodo GetBalance PortalWeb (ahora con tipoOperacion)

--- Controllers/PortalWebController.cs
+++ Controllers/PortalWebController.cs
@@ -167,7 +167,7 @@ namespace ApiRestDesarrollo.Controllers
         }
 
         [HttpGet("Balance")]
-        public ActionResult<IEnumerable<ComandRead>> GetBalance(int usuarioId)
+        public ActionResult<IEnumerable<HistoryOperationAccount>> GetBalance(int usuarioId)
         {
             var saldo = _portal.GetBalance(usuarioId);
             if (saldo != null)
