36d780e72a97824639bbbe87a45c434aedccafb7
dexter <linkdex456@gmail.com>
2020-08-17 01:56:18 -0400

    Se acomodo el get de reintegros

--- Dtos/Operation/ReadOperationReintegro.cs
+++ Dtos/Operation/ReadOperationReintegro.cs
@@ -7,10 +7,11 @@ namespace ApiRestDesarrollo.Dtos.Operation
 {
     public class ReadOperationReintegro
     {
+        public string UsuarioSolicitante { get; set; }
         public DateTime Fecha { get; set; }
         public decimal Monto { get; set; }
         public int Estatus { get; set; }
-        public string UsuarioSolicitante { get; set; }
         public string Referencia { get; set; }
+        
     }
 }
